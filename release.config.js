a494e253160623b811a0aa36b926e1889f6e0874
oluchiesomonu <mail4oluchi@gmail.com>
2023-10-07 15:15:56 -0400

    fix: modified workflow

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    branches: "main",
+    repositoryUrl: "https://github.com/oluchiesomonu/semanticS3.git",
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/git',
+      '@semantic-release/github']
+ }
+ 
